35f11b633da29cb6f7bdd7867f644cb3d3dc15df
hp <sidshekhawat26@gmail.com>
2025-07-06 11:21:07 +0530

    moved variables to .env file

--- models/index.js
+++ models/index.js
@@ -1,10 +1,17 @@
 // models/index.js
+require("dotenv").config();
+
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("blogdb", "postgres", "123456789", {
-  host: "localhost",
-  port: 5433,
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 const db = {};
 
